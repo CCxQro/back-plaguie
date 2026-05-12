c59ff9b9220aa370afce02eda28ef54bc1dd5dfd
David-Brnb <321171197@ciencias.unam.mx>
2026-05-11 22:02:50 -0600

    add: data for id user

--- src/main/java/itesm/mx/application/dto/LoginResponseDto.java
+++ src/main/java/itesm/mx/application/dto/LoginResponseDto.java
@@ -1,13 +1,15 @@
 package itesm.mx.application.dto;
 
 public class LoginResponseDto {
+    public Long userId;
     public String name;
     public String email;
     public Integer roleId;
     public Boolean isActive;
     public GetLocationResponseDto location;
 
-    public LoginResponseDto(String name, String email, Integer roleId) {
+    public LoginResponseDto(Long userId, String name, String email, Integer roleId) {
+        this.userId = userId;
         this.name = name;
         this.email = email;
         this.roleId = roleId;
